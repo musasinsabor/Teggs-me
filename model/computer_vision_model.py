82d09df30c54046a3c895b6dfde5d9ba80a9f963
musasinsabor <genesis@mammut.io>
2022-01-15 13:28:24 -0400

    model

--- model/computer_vision_model.py
+++ model/computer_vision_model.py
@@ -1,16 +1,17 @@
-from transformers import ViTFeatureExtractor, ViTForImageClassification
+from transformers import ViTFeatureExtractor, ViTForImageClassification, ViTConfig
 from typing import Dict
 from hugsvision.nnet.VisionClassifierTrainer import VisionClassifierTrainer
-from pathlib import Path
-from dataset_creation import CocoTeggsme
 import numpy as np
 import torch
+from torch.utils.data import DataLoader
 
 
 class EggsViTModel:
     """This is a computer vision model in ImageClassification task wrapped in huggingface."""
+
     def __init__(self,
-                 parameters):
+                 parameters,
+                 dataset):
         """It takes a parameters Dict where it can be specified the following data:
             model_name(str): Model name in huggingface/models storage.
             num_labels(int): Specification of the label numbers.
@@ -22,26 +23,25 @@ class EggsViTModel:
         self.model_name = parameters["model_name"]
         self.num_labels = parameters["num_labels"]
         self.model_training_parameters = parameters["model_training_parameters"]
-        self.dataset = self.dataset_obj()
-        self.model = self.pretrained_model()
         self.feature_extractor = ViTFeatureExtractor.from_pretrained(self.model_name)
+        self.dataset = self.dataset_obj(dataset)
+        self.model = self.pretrained_model()
+        self.my_dataset = dataset
 
-    def dataset_obj(self):
+    def dataset_obj(self, dataset):
         """
         This method uses the dataset info to create a dataset instance.
         :returns a Dataset object
         """
-        my_img_folder = Path("C:/Users/Genesis/Teggs-me/dataset/images")
-        my_ann_folder = Path("C:/Users/Genesis/Teggs-me/dataset/images/masks")
-        my_ann_file = Path("C:/Users/Genesis/Teggs-me/dataset/coco_instances.json")
-        dataset = CocoTeggsme(my_img_folder, my_ann_folder, my_ann_file, self.feature_extractor)
         return dataset
 
     def pretrained_model(self):
         """
         This method specify a pretrained ViModel.
         """
-        pt_model = ViTForImageClassification.from_pretrained(self.model_name, num_labels=len(self.num_labels))
+        config = ViTConfig.from_pretrained(self.model_name, ignore_mismatched_sizes=True)
+        config.num_labels = self.num_labels
+        pt_model = ViTForImageClassification(config)
         return pt_model
 
     def model_training(self):
@@ -57,10 +57,10 @@ class EggsViTModel:
             test=val_dataset,
             output_dir="./out/",
             lr=2e-5,
-            fp16=True,
             model=ViTForImageClassification.from_pretrained(
-                self.model,
+                self.model_name,
                 num_labels=3,
+                ignore_mismatched_sizes=True
             ),
             feature_extractor=self.feature_extractor,
         )
