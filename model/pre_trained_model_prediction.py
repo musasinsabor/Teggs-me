c5f0f07346a5e91991e04d634149900e9c95de82
musasinsabor <genesis@mammut.io>
2022-01-18 21:51:47 -0400

    app pretrained model classification

--- model/pre_trained_model_prediction.py
+++ model/pre_trained_model_prediction.py
@@ -1,8 +1,10 @@
 from transformers import DetrFeatureExtractor, DetrForObjectDetection
 from model.dataset_creation import CocoTeggsme
 from model.computer_vision_model import EggsViTModel
+from hugsvision.inference.VisionClassifierInference import VisionClassifierInference
 from PIL import Image
-
+import torch
+import matplotlib.pyplot as plt
 import os
 
 directory = os.path.dirname(os.path.realpath(__file__))
@@ -15,20 +17,21 @@ my_ann_folder = os.path.join(directory, my_data, my_masks)
 my_ann_file = os.path.join(directory, my_data, my_file)
 
 
-class PretrainedModelPrediction:
+class PretrainedObjectDetectionModelPrediction:
     """This is a computer vision model in ImageClassification task wrapped in huggingface."""
 
-    def __init__(self,
-                 model_name_path):
+    def __init__(self):
         """This is a prediction manager. Here are two possibilities:
         1. Make a prediction with a huggingaface pretrained model,
         2. Make a prediction with a custom model
         """
-        self.model_name = model_name_path
         self.img_class_pretrained = 'facebook/detr-resnet-101-dc5'
         self.model_output_path = "./my_model"
         self.feature_extractor_pretrained = DetrFeatureExtractor.from_pretrained(self.img_class_pretrained)
         self.model_pretrained = DetrForObjectDetection.from_pretrained(self.img_class_pretrained)
+        self.COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+                       [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+        self.id2label = self.model_pretrained.config.id2label
 
     def image_classification_classifier_pretrained(self, uploaded_file):
         """
@@ -38,11 +41,60 @@ class PretrainedModelPrediction:
         pixels = img.load()
         inputs = self.feature_extractor_pretrained(images=img, return_tensors="pt")
         outputs = self.model_pretrained(**inputs)
-
-        # model predicts bounding boxes and corresponding COCO classes
         logits = outputs.logits
         bboxes = outputs.pred_boxes
+
         return logits, bboxes
+
+    # for output bounding box post-processing
+    def box_cxcywh_to_xyxy(self, x):
+        x_c, y_c, w, h = x.unbind(1)
+        b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+             (x_c + 0.5 * w), (y_c + 0.5 * h)]
+        return torch.stack(b, dim=1)
+
+    def rescale_bboxes(self, out_bbox, size):
+        img_w, img_h = size
+        b = self.box_cxcywh_to_xyxy(out_bbox)
+        b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+        return b
+
+    def plot_results(self, pil_img, prob, boxes):
+        plt.figure(figsize=(16, 10))
+        plt.imshow(pil_img)
+        ax = plt.gca()
+        colors = self.COLORS * 100
+        for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
+            ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                       fill=False, color=c, linewidth=3))
+            cl = p.argmax()
+            text = f'{self.id2label[cl.item()]}: {p[cl]:0.2f}'
+            ax.text(xmin, ymin, text, fontsize=15,
+                    bbox=dict(facecolor='yellow', alpha=0.5))
+        plt.axis('off')
+        plt.show()
+
+    def visualize_predictions(self, image, outputs, threshold=0.9):
+        # keep only predictions with confidence >= threshold
+        probas = outputs.logits.softmax(-1)[0, :, :-1]
+        keep = probas.max(-1).values > threshold
+
+        # convert predicted boxes from [0; 1] to image scales
+        bboxes_scaled = self.rescale_bboxes(outputs.pred_boxes[0, keep].cpu(), image.size)
+
+        # plot results
+        results = self.plot_results(image, probas[keep], bboxes_scaled)
+        return results
+
+    def object_detection_classifier(self):
+        """
+        This method makes an image classification on a pretrained model.
+        """
+        classifier = VisionClassifierInference(
+            feature_extractor=self.feature_extractor_pretrained,
+            model=self.model_pretrained,
+        )
+        return classifier
 
     def image_classification_classifier_my_model(self, uploaded_file):
         """
--- teggs_me_app.py
+++ teggs_me_app.py
@@ -1,8 +1,7 @@
 # importing the libraries
 import streamlit as st
 from PIL import Image
-from hugsvision.inference.VisionClassifierInference import VisionClassifierInference
-from transformers import ViTFeatureExtractor, ViTForImageClassification
+from model.pre_trained_model_prediction import PretrainedObjectDetectionModelPrediction
 import os
 
 directory = os.path.dirname(os.path.realpath(__file__))
@@ -10,14 +9,7 @@ my_default_image = "eggs_in_the_nest.jpeg"
 my_img = os.path.join(directory, my_default_image)
 
 # loading the classifier
-
-pretrained_path = 'facebook/detr-resnet-101-dc5'
-classifier = VisionClassifierInference(
-    feature_extractor=ViTFeatureExtractor.from_pretrained(pretrained_path),
-    model=ViTForImageClassification.from_pretrained(pretrained_path),
-    )
-
-
+classifier = PretrainedObjectDetectionModelPrediction()
 # Designing the interface
 st.title("Teggs-me: Eggs in Nest Image Classification App")
 
@@ -33,43 +25,14 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 # Choose your own image
 uploaded_file = st.sidebar.file_uploader(" ", type=['png', 'jpg', 'jpeg'])
 
-if uploaded_file is not None:
+if uploaded_file is None:
+    st.sidebar.write("Please upload an Image to Classify")
+else:
+    st.sidebar.write("Your image is gonna start being classified")
     u_img = Image.open(uploaded_file)
     show.image(u_img, 'Uploaded Image', use_column_width=True)
-
-# For newline
-model = st.sidebar.selectbox(
-     'Select a model',
-     options=['pre_trained_model', 'my_model'])
-
-if st.sidebar.button("Click Here to Classify"):
-
-    if uploaded_file is None:
-
-        st.sidebar.write("Please upload an Image to Classify")
-
-    else:
-
-        if model == "pretrained_model":
-            with st.spinner('Pretrained Classifying ...'):
-
-                prediction = classifier.predict(img_path=uploaded_file)
-                st.success('Done!')
-
-            st.sidebar.header("Predicted class:", prediction)
-
-            # Formatted probability value to 3 decimal places
-            probability = "{:.3f}".format(float(prediction * 100))
-
-            # Classify cat being present in the picture if prediction > 0.5
-
-            if prediction > 0.5:
-
-                st.sidebar.write("It's a 'Cat' picture.", '\n')
-
-                st.sidebar.write('**Probability: **', probability, '%')
-
-            else:
-                st.sidebar.write(" It's a 'Non-Cat' picture ", '\n')
-
-                st.sidebar.write('**Probability: **', probability, '%')
+    with st.spinner('Pretrained Classifying ...'):
+        prediction = classifier.image_classification_classifier_pretrained(uploaded_file)
+        st.success('Done!')
+        st.sidebar.header("Predicted class:")
+        st.sidebar.write(prediction)
